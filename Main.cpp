5270aff473f6d7797d7dfbd53f1e8ad5bdbd1c12
Aaron Tabor <Shoggoth269@gmail.com>
2019-02-24 11:21:43 -0600

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,83 @@
+#include <Windows.h>
+
+#include <thread>
+#include <chrono>
+
+void LeftClick()
+{
+	INPUT Input = { 0 };
+	// left down 
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	::SendInput(1, &Input, sizeof(INPUT));
+
+	// left up
+	::ZeroMemory(&Input, sizeof(INPUT));
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	::SendInput(1, &Input, sizeof(INPUT));
+}
+
+int main()
+{
+
+	bool click = false;
+	std::chrono::milliseconds clickInterval = std::chrono::milliseconds(50);
+
+	while (true)
+	{
+		if (GetAsyncKeyState(VK_F6) & 1)
+			click = !click;
+
+		if (GetAsyncKeyState(VK_F7) & 1)
+			return 0;
+
+		if (click)
+		{
+			LeftClick();
+			std::this_thread::sleep_for(clickInterval);
+		}
+		else
+			std::this_thread::sleep_for(std::chrono::milliseconds(100)); //throttle the loop when not clicking
+	}
+
+	return 0;
+}
+
+//int main()
+//{
+//
+//	bool click = false;
+//	std::chrono::milliseconds clickInterval = std::chrono::milliseconds(100);
+//
+//	while (true)
+//	{
+//		if (GetAsyncKeyState(VK_F6))
+//			click = !click;
+//
+//		if (GetAsyncKeyState(VK_F7))
+//			return 0;
+//
+//		if (click)
+//		{
+//			while (true)
+//			{
+//				if (GetAsyncKeyState(VK_F6))
+//				{
+//					click = !click;
+//					break;
+//				}
+//
+//				if (GetAsyncKeyState(VK_F7))
+//					return 0;
+//
+//				LeftClick();
+//				std::this_thread::sleep_for(clickInterval);
+//			}
+//		}
+//		else
+//			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//	}
+//
+//	return 0;
+//}
